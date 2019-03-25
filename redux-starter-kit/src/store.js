79cfede45a3aca6d9f8a2d4de54e4ba6a232b95e
eunajjing <high.euna@gmail.com>
2019-03-25 14:08:58 +0900

    redux-thunk 적용

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(modules, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 /* 미들웨어가 여러 개일 때는 콤마로 주며 순서는 여기에서 전달한 파라미터 순.
 applyMiddleware(a, b, c);
 */
