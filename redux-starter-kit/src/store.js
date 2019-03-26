e13134c5a1891fd14ba0acd920f9496864993ed8
eunajjing <high.euna@gmail.com>
2019-03-26 09:36:26 +0900

    redux-promise-middleware

--- redux-starter-kit/src/store.js
+++ redux-starter-kit/src/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 import {createLogger} from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+import {createPromise} from 'redux-promise-middleware';
+// import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
+const pm = createPromise({
+    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+});
+
+const store = createStore(modules, applyMiddleware(logger, pm));
 /* 미들웨어가 여러 개일 때는 콤마로 주며 순서는 여기에서 전달한 파라미터 순.
 applyMiddleware(a, b, c);
 */
