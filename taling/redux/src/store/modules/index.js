2443f1591a2f9152deb7e48ee563dec278d2f9fc
eunajjing <k_eaaaaa@naver.com>
2019-06-10 22:06:07 +0900

    주석 일부 지움

--- taling/redux/src/store/modules/index.js
+++ taling/redux/src/store/modules/index.js
@@ -3,4 +3,3 @@ import counter from "./counter";
 import waiting from "./waiting";
 
 export default combineReducers({ counter, waiting });
-// 리듀서가 여러 개인 경우 모아서 스토어에 보냄
--- taling/redux/src/store/modules/waiting.js
+++ taling/redux/src/store/modules/waiting.js
@@ -5,20 +5,11 @@ const CREATE = "waiting/CREATE";
 const ENTER = "waiting/ENTER";
 const LEAVE = "waiting/LEAVE";
 
-// const changeInput = text => ({ type: CHANGE_INPUT, payload: text });
-// const create = name => ({ type: CREATE, payload: name });
-// const enter = id => ({ type: ENTER, payload: id });
-// const leave = id => ({ type: LEAVE, payload: id });
-
-// export const changeInput = createAction(CHANGE_INPUT);
-
 let id = 3;
 export const changeInput = createAction(CHANGE_INPUT, text => text);
 export const create = createAction(CREATE, name => ({ name, id: id++ }));
-// 리듀서 안에서 아이디 증가를 할 수 없기 때문에 액션 함수 내에서 처리
 export const enter = createAction(ENTER, id => id);
 export const leave = createAction(LEAVE, id => id);
-// 명시적으로 기재
 
 const initialState = {
   input: "",
@@ -41,11 +32,8 @@ const initialState = {
   ]
 };
 
-// 스위치의 블록 스코프는 스위치 안 모두
-
 export default handleActions(
   {
-    // 케이스와 거의 유사함
     [CHANGE_INPUT]: (state, action) => ({
       ...state,
       input: action.payload
