98412a5f6b32955c256ee2d0ab318cf8ee6fd7cf
eunajjing <k_eaaaaa@naver.com>
2019-06-17 17:15:53 +0900

    라우터 상태와 메뉴 동기화

---
+++ woowahan/navigation-03/src/router/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { DefaultLayout } from "../containers";
+import * as Pages from "../pages";
+
+interface IProps {}
+
+const Router: React.FC<IProps> = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <DefaultLayout>
+          <Switch>
+            <Route exact path="/" component={Pages.Dashboard} />
+            <Route exact path="/orders" component={Pages.Order} />
+            <Route component={Pages.PageNotFound} />
+          </Switch>
+        </DefaultLayout>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
